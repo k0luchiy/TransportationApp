cb7c7a9331c2d02b86b30b2308651741dac61b01
k0luchiy <antoshka.osipov.04@mail.ru>
2024-04-13 23:26:08 +0500

    Test structure for consideration

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
+
     QQmlApplicationEngine engine;
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
         &app, []() { QCoreApplication::exit(-1); },
--- src/Database/database.cpp
+++ src/Database/database.cpp
@@ -7,12 +7,13 @@ Database::Database()
 
 
 bool Database::connectToDatabase(){
-    this->addDatabase("QMYSQL");
-    this->setHostName("127.0.0.1");
-    this->setDatabaseName("TransportationDb");
-    this->setUserName("root");
-    this->setPassword("5555472Ao&");
-    this->setPort(3306);
+    //this->addDatabase("QMYSQL");
+    //this->setHostName("127.0.0.1");
+    //this->setDatabaseName("TransportationDb");
+    //this->setUserName("root");
+    //this->setPassword("5555472Ao&");
+    //this->setPort(3306);
 
+    this->setDatabaseName("Driver={MySQL ODBC 8.0 Unicode Driver};Server=localhost;Database=TransportationDB;Uid=root;Port=3306;Pwd=5555472Ao&;WSID=.");
     return this->open();
 }

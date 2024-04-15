7a7005243aad177e3e30eac3851bf04f94ca798a
k0luchiy <antoshka.osipov.04@mail.ru>
2024-04-15 15:55:57 +0500

    UserModel definition

--- src/UserModels/usermodel.cpp
+++ src/UserModels/usermodel.cpp
@@ -1,7 +1,61 @@
 #include "usermodel.h"
 
+//! Select query to get user info from database
+const char* UserModel::SELECT_QUERY =
+    " SELECT u.UserId, u.Email, u.FirstName, u.LastName, u.RoleId, ur.RoleTitle "
+    " from Users u  "
+    " join UserRoles ur on u.roleId = ur.roleId ";
+
 UserModel::UserModel(QObject *parent)
     : QSqlQueryModel{parent}
 {
-
+    setQuery(SELECT_QUERY);
 }
+
+//! Role names getter
+QHash<int, QByteArray> UserModel::roleNames() const
+{
+    return m_roleNames;
+}
+
+//! Generate role names from record columns
+void UserModel::generateRoleNames()
+{
+    m_roleNames.clear();
+    QSqlRecord empty_record = record();
+    for(size_t i=0; i < empty_record.count(); ++i){
+        m_roleNames.insert(Qt::UserRole + i +1, empty_record.fieldName(i).toUtf8());
+    }
+}
+
+//! Query setter
+void UserModel::setQuery(const QString& query, const QSqlDatabase& db)
+{
+    QSqlQueryModel::setQuery(query, db);
+    generateRoleNames();
+}
+
+//! Query setter
+void UserModel::setQuery(QString&& query)
+{
+    QSqlQueryModel::setQuery(std::move(query));
+    generateRoleNames();
+}
+
+//! Data getter by index and role
+QVariant UserModel::data(const QModelIndex& index, int role) const
+{
+    QVariant value;
+    if(role < Qt::UserRole){
+        value = QSqlQueryModel::data(index, role);
+        return value;
+    }
+
+    int columnInd = role - Qt::UserRole - 1;
+    QModelIndex modelIndex = this->index(index.row(), columnInd);
+    value = QSqlQueryModel::data(modelIndex, Qt::DisplayRole);
+
+    return value;
+}
+
+
--- src/UserModels/usermodel.h
+++ src/UserModels/usermodel.h
@@ -1,14 +1,43 @@
+/*! \file usermodel.h
+ * \brief Header file for user model
+ *
+ * Declaration of user model.
+ *
+*/
+
 #ifndef USERMODEL_H
 #define USERMODEL_H
 
-#include <QSqlQueryModel>
 #include <QObject>
+#include <QHash>
+#include <QVariant>
+#include <QSqlRecord>
+#include <QSqlQuery>
+#include <QSqlQueryModel>
 
+
+/*!
+ * \brief The UserModel class
+ *
+ * UserModel class store's user records
+ *
+ */
 class UserModel : public QSqlQueryModel
 {
     Q_OBJECT
 public:
     explicit UserModel(QObject *parent = nullptr);
+    QVariant data(const QModelIndex& ind, int role) const;
+    QHash<int, QByteArray> roleNames() const;
+    void setQuery(const QString& query, const QSqlDatabase& db = QSqlDatabase());
+    void setQuery(QString&& query);
+
+private:
+    void generateRoleNames();
+
+private:
+    const static char* SELECT_QUERY;
+    QHash<int, QByteArray> m_roleNames;
 };
 
 #endif // USERMODEL_H

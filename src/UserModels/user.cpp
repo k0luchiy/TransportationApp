5d0016dcfe4633ef4c6d27dd3878dc45d2656b37
k0luchiy <antoshka.osipov.04@mail.ru>
2024-04-18 01:01:04 +0500

    User component implementation

--- src/UserModels/user.cpp
+++ src/UserModels/user.cpp
@@ -1,7 +1,80 @@
+/*! \file user.cpp
+ *  \brief User class source file.
+ *
+ *  This file contains User class implementation.
+ *
+ */
+
 #include "user.h"
 
 User::User(QObject *parent)
     : QObject{parent}
 {
 
+}
+
+
+User::User(const User& other)
+{
+    quint64 userId = other.userId();
+    QString email = other.email();
+    QString firstName = other.firstName();
+    QString lastName = other.lastName();
+    quint64 roleId = other.roleId();
+    QString roleTitle = other.roleTitle();
+    quint64 rolePriority = other.rolePriority();
+    bool isAuthenticated = other.isAuthenticated();
+
+    setUserData(userId, email, firstName, lastName, roleId, roleTitle, rolePriority, isAuthenticated);
+}
+
+
+//
+// Getters
+//
+quint64 User::userId() const { return m_userId; } //!< Getter for userId field
+QString User::firstName() const { return m_firstName; } //!< Getter for firstName field
+QString User::lastName() const { return m_lastName; } //!< Getter for lastName field
+QString User::email() const { return m_email; } //!< Getter for email field
+quint64 User::roleId() const { return m_roleId; } //!< Getter for roleId field
+QString User::roleTitle() const { return m_roleTitle; } //!< Getter for roleTitle field
+quint64 User::rolePriority() const { return m_rolePriority; } //!< Getter for rolePriority field
+bool User::isAuthenticated() const { return m_isAuthenticated; } //!< Getter for roleTitle field
+
+//
+// Setters
+//
+void User::setUserId(quint64 userId)  { m_userId = userId; emit userIdChanged(); } //!< Setter for userId field, emmiting userIdChanged signal
+void User::setFirstName(const QString& firstName) { m_firstName = firstName; emit firstNameChanged(); } //!< Setter for firstName field, emmiting firstNameChanged signal
+void User::setLastName(const QString& lastName) { m_lastName = lastName; emit lastNameChanged(); } //!< Setter for lastName field, emmiting lastNameChanged signal
+void User::setEmail(const QString& email) { m_email = email; emit emailChanged(); } //!< Setter for email field, emmiting emailChanged signal
+void User::setRoleId(quint64 roleId) { m_roleId = roleId; emit roleIdChanged(); } //!< Setter for roleId field, emmiting roleIdChanged signal
+void User::setRoleTitle(const QString& roleTitle) { m_roleTitle = roleTitle; emit roleTitleChanged(); } //!< Setter for roleTitle field, emmiting roleTitleChanged signal
+void User::setRolePriority(quint64 rolePriority) { m_rolePriority = rolePriority; emit rolePriorityChanged(); } //!< Setter for roleId field, emmiting roleIdChanged signal
+void User::setIsAuthenticated(bool isAuthenticated) { m_isAuthenticated = isAuthenticated; emit isAuthenticatedChanged(); } //!< Setter for isAuthenticated field, emmiting isAuthenticatedChanged signal
+
+
+/*! \brief Setter for all user data, calls all user's fields setters
+ *  @param userId user id to set
+ *  @param email user's email to set
+ *  @param firstName user's first name to set
+ *  @param lastName user's last name to set
+ *  @param roleId user's role id to set
+ *  @param roleTitle user's role title to set
+ *  @param rolePriority role's priority to set, identifies users rights
+ *  @param isAuthenticated sets if current user authenticated or nor.
+ */
+void User::setUserData(quint64 userId, const QString& email,
+                 const QString& firstName, const QString& lastName,
+                 quint64 roleId, const QString& roleTitle,
+                 quint64 rolePriority, bool isAuthenticated)
+{
+    setUserId(userId);
+    setEmail(email);
+    setFirstName(firstName);
+    setLastName(lastName);
+    setRoleId(roleId);
+    setRoleTitle(roleTitle);
+    setRolePriority(rolePriority);
+    setIsAuthenticated(isAuthenticated);
 }
--- src/UserModels/user.h
+++ src/UserModels/user.h
@@ -1,15 +1,96 @@
+/*! \file user.h
+ *  \brief Header file for user component.
+ *
+ *  Declaration of all properties of user component.
+ *
+*/
+
 #ifndef USER_H
 #define USER_H
 
 #include <QObject>
+#include <QSqlRecord>
 
+/*!
+ * \brief Class defining user component.
+ *
+ *  Class for represantation of a user connects qml frontend with backend.
+ *  Contains all properties of a user.
+ *  Properties working through Q_PROPERTY macros.
+ *
+ */
 class User : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(quint64 userId READ userId WRITE setUserId NOTIFY userIdChanged) //!< User's id property to identify each user
+    Q_PROPERTY(QString email READ email WRITE setEmail NOTIFY emailChanged) //!< Email of a user
+    Q_PROPERTY(QString firstName READ firstName WRITE setFirstName NOTIFY firstNameChanged) //!< User's first name
+    Q_PROPERTY(QString lastName READ lastName WRITE setLastName NOTIFY lastNameChanged) //!< User's last name
+    Q_PROPERTY(quint64 roleId READ roleId WRITE setRoleId NOTIFY roleIdChanged) //!< Role id of a user to identify role
+    Q_PROPERTY(QString roleTitle READ roleTitle WRITE setRoleTitle NOTIFY roleTitleChanged) //!< Role title
+    Q_PROPERTY(quint64 rolePriority READ rolePriority WRITE setRolePriority NOTIFY rolePriorityChanged) //!< Role id of a user to identify role
+    Q_PROPERTY(bool isAuthenticated READ isAuthenticated WRITE setIsAuthenticated NOTIFY isAuthenticatedChanged) //!< Role title
+
+private:
+    quint64 m_userId = 0;       //!< User id
+    QString m_email= "";        //!< User email
+    QString m_firstName = "";   //!< User first name
+    QString m_lastName = "";    //!< User last name
+    quint64 m_roleId = 0;       //!< Role id of a user to identify role
+    QString m_roleTitle = "";   //!< Role title
+    quint64 m_rolePriority = 0;   //!< Role priority to set users rights
+    bool m_isAuthenticated = false; //!< Is current user authenticated or not
+
 public:
     explicit User(QObject *parent = nullptr);
+    User(const User& other);
+    User(User&& other) = default;
 
+public Q_SLOTS:
+    void setUserData(quint64 userId, const QString& email,
+                     const QString& firstName, const QString& lastName,
+                     quint64 roleId, const QString& roleTitle,
+                     quint64 rolePriority, bool isAuthenticated);
+//
+// Getters
+//
+public:
+    quint64 userId() const;
+    QString email() const;
+    QString firstName() const;
+    QString lastName() const;
+    quint64 roleId() const;
+    QString roleTitle() const;
+    quint64 rolePriority() const;
+    bool isAuthenticated() const;
+
+//
+// Setters
+//
+public:
+    void setUserId(quint64 userId);
+    void setEmail(const QString& email);
+    void setFirstName(const QString& firstName);
+    void setLastName(const QString& lastName);
+    void setRoleId(quint64 roleId);
+    void setRoleTitle(const QString& roleeTitle);
+    void setRolePriority(quint64 rolePriority);
+    void setIsAuthenticated(bool isAuthenticated);
+
+
+//
+// Property's signals
+//
 signals:
+    void userIdChanged();       //!< Emits signal if userId changed
+    void emailChanged();        //!< Emits signal if email changed
+    void firstNameChanged();    //!< Emits signal if firstName changed
+    void lastNameChanged();     //!< Emits signal if lastName changed
+    void roleIdChanged();       //!< Emits signal if roleId changed
+    void roleTitleChanged();    //!< Emits signal if roleTitle changed
+    void rolePriorityChanged();       //!< Emits signal if rolePriority changed
+    bool isAuthenticatedChanged();  //!< Emits signal if isAuthenticated changed
+
 
 };
 

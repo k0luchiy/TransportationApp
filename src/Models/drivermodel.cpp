cfcbbb8b7069a4de036c3429498998c49602dd8b
k0luchiy <antoshka.osipov.04@mail.ru>
2024-04-30 17:30:08 +0500

    Driver model

--- src/Models/drivermodel.cpp
+++ src/Models/drivermodel.cpp
@@ -1,7 +1,17 @@
 #include "drivermodel.h"
 
+//! Query whitch AbstractSqlQueryModel will execute for selecting all drivers information.
+const char* DriverModel::SELECT_QUERY =
+    "select dr.DriverId, pi.FirstName, pi.MiddleName, pi.LastName, \
+    pi.BirthDate, sc.ScheduleId, sc.ScheduleTitle, \
+    dr.DrivingCategory, dr.Salary, dr.Experience \
+    from Drivers dr \
+    join PersonalInfo pi on dr.PersonId = pi.PersonId   \
+    join Schedules sc on dr.ScheduleId = sc.ScheduleId";
+
+
 DriverModel::DriverModel(QObject *parent)
-    : QSqlQueryModel{parent}
+    : AbstractSqlQueryModel{SELECT_QUERY, parent}
 {
 
 }
--- src/Models/drivermodel.h
+++ src/Models/drivermodel.h
@@ -10,11 +10,21 @@
 #define DRIVERMODEL_H
 
 #include <QSqlQueryModel>
+#include "abstractsqlquerymodel.h"
 
-class DriverModel : public QSqlQueryModel
+/*!
+ * \brief Stores drivers records.
+ *
+ *  DriverModel class store's drives records after executing query.
+ *
+ */
+class DriverModel : public AbstractSqlQueryModel
 {
 public:
     explicit DriverModel(QObject *parent = nullptr);
+
+private:
+    const static char* SELECT_QUERY;
 };
 
 #endif // DRIVERMODEL_H

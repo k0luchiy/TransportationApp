f6fd7858d00c89e93d395f688b91b8c701838776
k0luchiy <antoshka.osipov.04@mail.ru>
2024-04-30 17:31:34 +0500

    Cars model

---
+++ src/Models/carsmodel.cpp
@@ -0,0 +1,62 @@
+/*! \file carsmodel.cpp
+ *  \brief CarsModel class source file.
+ *
+ *  This file contains CarsModel class implementation.
+ *
+ */
+
+#include "carsmodel.h"
+
+const char* CarsModel::SELECT_QUERY =
+    "select \
+        c.CarId, c.CarType, c.CarModel, \
+        c.CarNumber, c.DrivingCategory, \
+        c.VolumeCapacity, c.Weightcapacity, \
+        c.LastInspection, c.ReleaseYear \
+    from Cars c";
+
+
+CarsModel::CarsModel(QObject *parent)
+    : AbstractSqlQueryModel{SELECT_QUERY, parent}
+{
+
+}
+
+
+/*!
+ * \brief Updates car's data by car id
+ * \param carId Id of a car to update
+ * \param carType   New car type to set
+ * \param carModel  New car model to set
+ * \param carNumber New car number to set
+ * \param volumeCapacity    New volume capacity to set
+ * \param weightCapacity    New weight capacity to set
+ * \param drivingCateogry   New driving category to set
+ * \return True if query executed successfully, False otherwise
+ */
+bool CarsModel::updateCar(
+        quint64 carId, const QString& carType, const QString& carModel,
+        const QString& carNumber, quint64 volumeCapacity, quint64 weightCapacity,
+        const QString& drivingCateogry
+)
+{
+    QSqlQuery query;
+    const QString reg_query =
+        " Update Cars set  \
+            carType = :carType, carModel = :carModel, \
+            carNumber = :carNumber, volumeCapacity = :volumeCapacity, \
+            weightCapacity = :weightCapacity,   drivingCateogry = :drivingCateogry \
+          where carId = :carId \
+        ";
+
+    query.prepare(reg_query);
+    query.bindValue(":carId", carId);
+    query.bindValue(":carType", carType);
+    query.bindValue(":carModel", carModel);
+    query.bindValue(":carNumber", carNumber);
+    query.bindValue(":volumeCapacity", volumeCapacity);
+    query.bindValue(":weightCapacity", weightCapacity);
+    query.bindValue(":drivingCateogry", drivingCateogry);
+
+    return query.exec();
+}
---
+++ src/Models/carsmodel.h
@@ -0,0 +1,36 @@
+/*! \file carsmdoel.h
+ *  \brief Header file for cars model
+ *
+ *  Declaration of cars model class.
+ *
+*/
+
+#ifndef CARSMODEL_H
+#define CARSMODEL_H
+
+#include "abstractsqlquerymodel.h"
+#include <QSqlQuery>
+
+/*!
+ * \brief The CarsModel class stores cars data.
+ *
+ * CarsModel class store's cars records after executing query.
+ *
+ */
+class CarsModel : public AbstractSqlQueryModel
+{
+public:
+    explicit CarsModel(QObject *parent = nullptr);
+
+public Q_SLOTS:
+    bool updateCar(
+        quint64 carId, const QString& carType, const QString& carModel,
+        const QString& carNumber, quint64 volumeCapacity, quint64 weightCapacity,
+        const QString& drivingCateogry
+    );
+
+private:
+    const static char* SELECT_QUERY;
+};
+
+#endif // CARSMODEL_H

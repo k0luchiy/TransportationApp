1683595ca532436111ca464d5764110955d3efd5
k0luchiy <antoshka.osipov.04@mail.ru>
2024-05-01 18:52:36 +0500

    Driving categories model

---
+++ src/Models/drivingcategoriesmodel.cpp
@@ -0,0 +1,20 @@
+/*! \file drivingcategoriesmodel.cpp
+ *  \brief DrivingCategoriesModel class source file.
+ *
+ *  This file contains DrivingCategoriesModel class implementation.
+ *
+ */
+
+#include "drivingcategoriesmodel.h"
+
+//! Query whitch AbstractSqlQueryModel will execute for selecting all user information.
+const char* DrivingCategoriesModel::SELECT_QUERY =
+    "select CategoryName, FullName, CategoryPriority \
+    from DrivingCategories \
+    order by CategoryPriority";
+
+DrivingCategoriesModel::DrivingCategoriesModel(QObject *parent)
+    : AbstractSqlQueryModel{SELECT_QUERY, parent}
+{
+
+}
---
+++ src/Models/drivingcategoriesmodel.h
@@ -0,0 +1,30 @@
+/*! \file drivingcategoriesmodel.h
+ *  \brief Header file for driving categories
+ *
+ *  Declaration of DrivingCategoryModel class.
+ *
+*/
+
+#ifndef DRIVINGCATEGORIESMODEL_H
+#define DRIVINGCATEGORIESMODEL_H
+
+#include "abstractsqlquerymodel.h"
+
+
+/*!
+ * \brief Stores driving categories.
+ *
+ * DrivingCategoriesModel class store's categories after executing query.
+ * Mainly for comboboxes.
+ *
+ */
+class DrivingCategoriesModel : public AbstractSqlQueryModel
+{
+public:
+    explicit DrivingCategoriesModel(QObject *parent = nullptr);
+
+private:
+    const static char* SELECT_QUERY;
+};
+
+#endif // DRIVINGCATEGORIESMODEL_H

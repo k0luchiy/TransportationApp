691fc01acd2f94bb36d291c5343def44ce1c636e
k0luchiy <antoshka.osipov.04@mail.ru>
2024-05-01 18:53:58 +0500

    Orders model

---
+++ src/Models/ordersmodel.cpp
@@ -0,0 +1,51 @@
+/*! \file ordersmodel.cpp
+ *  \brief OrdersModel class source file.
+ *
+ *  This file contains OrdersModel class implementation.
+ *
+ */
+
+#include "ordersmodel.h"
+
+
+//! Query whitch AbstractSqlQueryModel will execute for selecting all user information.
+const char* OrdersModel::SELECT_QUERY =
+    "select \
+        o.OrderId, o.CreatedDate, o.AskedDeliveryDate,  \
+        s.StatusId, s.StatusTitle, o.Cost,  \
+        o.Address, o.Volume, o.Weight   \
+    from Orders o   \
+    join OrderStatus s on o.StatusId = s.StatusId ";
+
+
+OrdersModel::OrdersModel(QObject *parent)
+    : AbstractSqlQueryModel{SELECT_QUERY, parent}
+{
+
+}
+
+
+bool OrdersModel::updateOrder(
+    quint64 orderId, const QDate& askedDeliveryDate,
+    quint64 statusId, float cost, const QString& address,
+    quint64 volume, quint64 weight
+)
+{
+    QSqlQuery query;
+    const QString reg_query =
+        "U pdate Orders set \
+            askedDeliveryDate = :askedDeliveryDate, statusId = :statusId,   \
+            cost = :cost, address = :address, volume = :volume, weight = :weight    \
+        where orderId = :orderId";
+
+    query.prepare(reg_query);
+    query.bindValue(":orderId", orderId);
+    query.bindValue(":askedDeliveryDate", askedDeliveryDate);
+    query.bindValue(":statusId", statusId);
+    query.bindValue(":cost", cost);
+    query.bindValue(":address", address);
+    query.bindValue(":volume", volume);
+    query.bindValue(":weight", weight);
+
+    return query.exec();
+}
---
+++ src/Models/ordersmodel.h
@@ -0,0 +1,38 @@
+/*! \file ordersmodel.h
+ *  \brief Header file for order model
+ *
+ *  Declaration of order model class.
+ *
+*/
+
+#ifndef ORDERSMODEL_H
+#define ORDERSMODEL_H
+
+#include "abstractsqlquerymodel.h"
+#include <QDate>
+
+
+/*!
+ * \brief Stores orders records.
+ *
+ * OrdersModel class store's orders records after executing query.
+ *
+ */
+class OrdersModel : public AbstractSqlQueryModel
+{
+public:
+    explicit OrdersModel(QObject *parent = nullptr);
+
+
+public Q_SLOTS:
+    bool updateOrder(
+        quint64 orderId, const QDate& askedDeliveryDate,
+        quint64 statusId, float cost, const QString& address,
+        quint64 volume, quint64 weight
+    );
+
+private:
+    const static char* SELECT_QUERY;
+};
+
+#endif // ORDERSMODEL_H

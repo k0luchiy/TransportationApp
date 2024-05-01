33249a043bedd251902ca0ed0b176fb3ca0dca94
k0luchiy <antoshka.osipov.04@mail.ru>
2024-05-01 18:53:24 +0500

    Order status model

---
+++ src/Models/orderstatusmodel.cpp
@@ -0,0 +1,18 @@
+/*! \file orderstatusmodel.cpp
+ *  \brief OrderStatusModel class source file.
+ *
+ *  This file contains OrderStatusModel class implementation.
+ *
+ */
+
+#include "orderstatusmodel.h"
+
+//! Query whitch AbstractSqlQueryModel will execute for selecting all user information.
+const char* OrderStatusModel::SELECT_QUERY =
+    "Select StatusId, StatusTitle from OrderStatus order by StatusId";
+
+OrderStatusModel::OrderStatusModel(QObject *parent)
+    : AbstractSqlQueryModel{SELECT_QUERY, parent}
+{
+
+}
---
+++ src/Models/orderstatusmodel.h
@@ -0,0 +1,30 @@
+/*! \file orderstatusmodel.h
+ *  \brief Header file for order status.
+ *
+ *  Declaration of OrderStatusModel class.
+ *
+*/
+
+#ifndef ORDERSTATUSMODEL_H
+#define ORDERSTATUSMODEL_H
+
+#include "abstractsqlquerymodel.h"
+
+
+/*!
+ * \brief Stores status records.
+ *
+ * OrderStatusModel class store's status records after executing query.
+ * Mainly for comboboxes.
+ *
+ */
+class OrderStatusModel : public AbstractSqlQueryModel
+{
+public:
+    explicit OrderStatusModel(QObject *parent = nullptr);
+
+private:
+    const static char* SELECT_QUERY;
+};
+
+#endif // ORDERSTATUSMODEL_H
